30ab44af629ea2a4e3baa8633e0c69f7e8fd84ea
Eiyub Bodur <71319621+jeuub@users.noreply.github.com>
2022-01-14 01:34:08 +0700

    cors allow

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./src/router");
-const bodyParser = require("body-parser")
-const db = require('./src/db')
+const bodyParser = require("body-parser");
+const db = require('./src/db');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
